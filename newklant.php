85d10e05a260323a9656c2a1d6766beb9a8e3011
Roy Dijkstra <roy.dijkstra@live.nl>
2018-12-20 11:26:13 +0100

    Removed some stuff

--- newklant.php
+++ newklant.php
@@ -123,8 +123,6 @@
   <!-- Include all compiled plugins (below), or include individual files as needed -->
   <script src="assets/js/bootstrap.min.js"></script>
   <script type="text/javascript" src="assets/js/jquery.min.js"></script>
-  <script type="text/javascript" src="assets/js/Chart.min.js"></script>
-  <script type="text/javascript" src="assets/js/chart.js"></script>
   <script src="assets/bootstrap-select/dist/bootstrap-select.min.js"></script>
   <script src="assets/js/countrypicker.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/js/bootstrap-datepicker.min.js"></script>

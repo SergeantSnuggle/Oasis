65b6eb0a2a2b91c65669beb1429a115b98cfedda
Roy Dijkstra <roy.dijkstra@live.nl>
2019-01-09 17:08:34 +0100

    Connectie met de oege database gemaakt. En de gemaakt dat de navbar aangeeft waar je momenteel zit

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-	$mysqli = new mysqli("localhost", "root", "", "projectfys");
+	$mysqli = new mysqli("oege.ie.hva.nl", "winsens", 'Zy$GNu/IDQi#JA', "zwinsens");
 	if ($mysqli->connect_errno) {
 		printf("Connect failed: %s\n", $mysqli->connect_error);
 		exit();
